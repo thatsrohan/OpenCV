b8f78f03f01d4d27da85ca8e0cb966a70d5960a7
thatsrohan <rg2oob@gmail.com>
2025-05-09 11:10:59 +0530

    Add files via upload

---
+++ ObjectTracking.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+tracker=cv2.TrackerGOTURN_create()
+cap=cv2.VideoCapture(r'C:\Users\LENOVO\Downloads\ObjTrackingTest.mp4')
+if not cap.isOpened():
+        print("no video file found")
+        frame=cap.read()
+        cv2.namedWindow('bbox')
+        bbox=cv2.selectROI('bbox',frame)
+        ok=tracker.init(frame,bbox)
+        cap.release
+        cv2.destroyAllWindows()
+        while True:
+                ret=cap.read()
+                if not ret:
+                        break
+                ok,bbox=tracker.update(frame)
+if ok:
+                top_left_coordinates=(bbox[0],bbox[1])
+                bottom_right_coordinates=(bbox[0]+bbox[2],bbox[1]-bbox[3])
+                cv2.rectangle(frame,p1,p2,(255,0,0),2,1)
+                
+
+                
